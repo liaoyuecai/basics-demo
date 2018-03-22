2e00097b94d7d364b19facd70e4e377cd852fa32
liaoyuecai <33653091+liaoyuecai@users.noreply.github.com>
2018-03-22 17:13:33 +0800

    save some test

--- src/main/java/concurrence/thread/ThreadPoolExecutorTest.java
+++ src/main/java/concurrence/thread/ThreadPoolExecutorTest.java
@@ -53,3 +53,75 @@ public class ThreadPoolExecutorTest {
 
     }
 }
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Transaction {
+    List<TransactionMessage> execute = new ArrayList();
+    int status = 0;
+    public Transaction addOperation(String queue, String operation, Object params) {
+        execute.add(new TransactionMessage(queue, operation, JSON.toJSONString(params)));
+        return this;
+    }
+
+    public Transaction addOperation(String queue, String operation, Object params, String remark) {
+        execute.add(new TransactionMessage(queue, remark, operation, JSON.toJSONString(params)));
+        return this;
+    }
+
+    public String getCurrentOperation() {
+        return execute.get(0).operation;
+    }
+
+    public <T> T getParams(Class<T> clazz) {
+        return JSONObject.parseObject(execute.get(0).params, clazz);
+    }
+
+    void complete(){
+        execute.get(0).success();
+        execute.remove(0);
+        if (execute.isEmpty())
+            status = 1;
+    }
+
+    void defeated(){
+        execute.get(0).fail();
+    }
+}
+import java.io.Serializable;
+import java.util.UUID;
+
+class TransactionMessage<T> implements Serializable{
+    String id = UUID.randomUUID().toString();
+    String queue;
+    String remark;
+    String operation;
+    String params;
+    int status = 0;
+
+    TransactionMessage(String queue, String operation, String params) {
+        this.queue = queue;
+        this.operation = operation;
+        this.params = params;
+    }
+
+    TransactionMessage(String queue, String remark, String operation, String params) {
+        this.queue = queue;
+        this.remark = remark;
+        this.operation = operation;
+        this.params = params;
+    }
+
+    void success(){
+        status = 1;
+    }
+
+    void fail(){
+        status = 2;
+    }
+
+}
